0937453109e6d791eb7daea72b8184d0e2025e55
Aditya Sharma <aditya.sharma@netsolutions.com>
2022-11-16 17:41:34 +0530

    Changed the Boundfield import

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description = open('README.rst').read() + '\n\n' + open('CHANGES.rst').read
 
 setup(
     name='django-form-utils',
-    version='2.0.0.post%s' % hgrev,
+    version='2.0.1.post%s' % hgrev,
     description='Form utilities for Django',
     long_description=long_description,
     author='Carl Meyer',
